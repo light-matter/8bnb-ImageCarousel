490709b0ea75713acf407e0abe72f3391157bb8a
William Chen <williamchen13225@gmail.com>
2020-01-24 13:39:38 -0800

    Connect server to frontend

--- server/index.js
+++ server/index.js
@@ -4,6 +4,12 @@ const bodyParser = require('body-parser');
 const app = express()
 const port = 3000
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 //serves up files in the public folder
 app.use(express.static('public'));
 
