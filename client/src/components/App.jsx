b9405ae42c495c07bb7e3df304bc31617064d1f1
William Chen <williamchen13225@gmail.com>
2020-01-24 10:17:09 -0800

    Create App/Index static components

---
+++ client/src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends from React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return(
+      <h2> Hello world! </h2>
+    )
+  }
+}
+
+
+
+export default App;
