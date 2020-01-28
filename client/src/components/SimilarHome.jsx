5a07d21d9ae2af7a5dc4e7c697d737153d03f248
William Chen <williamchen13225@gmail.com>
2020-01-28 11:41:46 -0800

    Functional Arrow, SimilarHome and SimilarHomes components

--- client/src/components/SimilarHome.jsx
+++ client/src/components/SimilarHome.jsx
@@ -1,11 +1,54 @@
 import React from 'react';
+import Arrow from './Arrow.jsx';
 
-var SimilarHome = ({url}) => (
-  <span>
+class SimilarHome extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentImgIdx: 0
+    }
 
-  <img src = {url} height="250" width="350"/>
+    this.previousSlide = this.previousSlide.bind(this);
+    this.nextSlide = this.nextSlide.bind(this);
+  }
 
-  </span>
-)
+  previousSlide () {
+    const lastIndex = this.props.similarHome.images.length - 1;
+    const { currentImgIdx } = this.state;
+    const shouldResetIndex = currentImgIdx === 0;
+    const index =  shouldResetIndex ? lastIndex : currentImgIdx - 1;
+
+    this.setState({
+      currentImgIdx: index
+    });
+  }
+
+  nextSlide () {
+    const lastIndex = this.props.similarHome.images.length - 1;
+    const { currentImgIdx } = this.state;
+    const shouldResetIndex = currentImgIdx === lastIndex;
+    const index =  shouldResetIndex ? 0 : currentImgIdx + 1;
+
+    this.setState({
+      currentImgIdx: index
+    });
+  }
+
+  render() {
+    return(
+      <div className = "similarHome">
+
+      <Arrow direction = "left" handleSubmit = {this.previousSlide} glyph = "&#9664;" />
+
+      <img src = {this.props.similarHome.images[this.state.currentImgIdx]} height = '250' width = '350'></img>
+
+      <Arrow direction = "right" handleSubmit = {this.nextSlide} glyph="&#9654;"/>
+
+      </div>
+    )
+  }
+
+}
+
 
 export default SimilarHome;
--- client/src/components/SimilarHomes.jsx
+++ client/src/components/SimilarHomes.jsx
@@ -1,57 +1,13 @@
 import React from 'react';
 import SimilarHome from './SimilarHome.jsx';
 
-class SimilarHomes extends React.Component{
-  constructor(props){
-    super(props)
-    this.state = {
-      currentImgIdx : 0
-    }
+const SimilarHomes = ({similarHomes}) => (
 
-    this.previousSlide = this.previousSlide.bind(this);
-    this.nextSlide = this.nextSlide.bind(this);
-  }
+  similarHomes.map((similarHome,i) =>
+    <SimilarHome similarHome={similarHome} key={i}/>
+  )
 
-  previousSlide () {
-    const lastIndex = imgUrls.length - 1;
-    // imgUrls.length - 1
-    const { currentImageIndex } = this.state;
-    const shouldResetIndex = currentImageIndex === 0;
-    const index =  shouldResetIndex ? lastIndex : currentImageIndex - 1;
+)
 
-    this.setState({
-      currentImageIndex: index
-    });
-  }
-
-  nextSlide () {
-    const lastIndex = imgUrls.length - 1;
-    imgUrls.length - 1
-    const { currentImageIndex } = this.state;
-    const shouldResetIndex = currentImageIndex === lastIndex;
-    const index =  shouldResetIndex ? 0 : currentImageIndex + 1;
-
-    this.setState({
-      currentImageIndex: index
-    });
-  }
-
-  render(){
-
-    return(
-      <div className = "similarHomes">
-
-      <Arrow direction = "left" handleSubmit = {this.previousSlide} glyph = "&#9664;" />
-
-        {this.props.similarHomes.map((similarHome,i) =>
-          <SimilarHome url={similarHome.images[this.state.currentImgIdx]} key={i}/>
-        )}
-
-      <Arrow direction = "right" handleSubmit = {this.nextSlide} glyph="&#9654;"/>
-
-      </div>
-    )
-  }
-}
 
 export default SimilarHomes
