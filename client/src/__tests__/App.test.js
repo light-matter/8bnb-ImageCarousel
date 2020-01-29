1ae5cd813a803584791663cf1ab1705a196aeb63
William Chen <williamchen13225@gmail.com>
2020-01-29 10:18:23 -0800

    4 tests on App

--- client/src/__tests__/App.test.js
+++ client/src/__tests__/App.test.js
@@ -2,15 +2,42 @@ import App from '../components/App.jsx';
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
+import axios from 'axios';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
 // the describe function is the test suite
-describe('checks if App is rendering properly', function() {
-  it('renders app component without crashing', function() {
-    const wrapper = shallow(<App />);
-    // console.log(wrapper.debug());
+describe('<App/>', () => {
+  it('Checks if App component is rendering without crashing', () => {
+    const wrapper = shallow(<App/>);
   });
+
+  it('Renders one <SimilarHomes/> component', () => {
+    const wrapper = shallow(<App/>);
+    expect(wrapper.find('SimilarHomes')).toHaveLength(1);
+  });
+
+  it('Has a state that is defined', () => {
+    const wrapper = shallow(<App/>);
+    axios.get('/homes')
+      .then(response => {
+        wrapper.setState({
+          homes: response.data[0].homes
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    expect(wrapper.state('homes')).toBeDefined();
+  });
+
+  it('Calls componentDidMount', () => {
+    jest.spyOn(App.prototype, 'componentDidMount');
+    const wrapper = shallow(<App/>);
+    expect(App.prototype.componentDidMount.mock.calls.length).toBe(1);
+  });
+
   
+
 
 });
--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,25 +2,23 @@ import React from 'react';
 import axios from 'axios';
 import SimilarHomes from './SimilarHomes.jsx';
 
-class App extends React.Component{
+class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       homes: []
-    }
-    // console.log(this.state.image);
+    };
   }
 
   getHomes() {
     axios.get('/homes')
       .then(response => {
         // handle success
-        console.log(response.data[0].homes);
+        // console.log(response.data[0].homes);
         // console.log(response.data[0].homes[0].images[0]);
         this.setState({
           homes: response.data[0].homes
-          // image: response.data[0].homes[0].images[0]
-        })
+        });
       })
       .catch(function (error) {
         // handle error
@@ -36,11 +34,9 @@ class App extends React.Component{
   }
 
   render() {
-    return(
-      <span>
-        <SimilarHomes similarHomes={this.state.homes}/>
-      </span>
-    )
+    return (
+      <SimilarHomes similarHomes={this.state.homes}/>
+    );
   }
 }
 
