9575c9d7d84b4a3a1ff43a6f247f438b84742524
William Chen <williamchen13225@gmail.com>
2020-02-01 17:39:04 -0800

    Add OuterArrow Component

---
+++ client/src/components/OuterArrow.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const RightArrow = styled.i`
+  transform: rotate(-45deg);
+  border: solid black;
+  border-width: 0 2px 2px 0;
+  display: inline-block;
+  padding: 3px;
+`;
+
+const LeftArrow = styled.i`
+    transform: rotate(135deg);
+    border: solid black;
+    border-width: 0 2px 2px 0;
+    display: inline-block;
+    padding: 3px;
+`;
+
+const Button = styled.button`
+  /* display: ${(props) => (props.showButtons ? 'block' : 'none')};
+  position: absolute;
+  width: 30px;
+  height: 30px;
+  top: 125px;
+  border-radius: 50px;
+  right: ${(props) => (props.direction === 'right' ? '10px' : '310px')}; */
+  top: 125px;
+`;
+
+const OuterArrow = ({ direction, handleSubmit, showButtons }) => (
+
+    <Button onClick={handleSubmit} direction={direction} showButtons={showButtons}>
+      {direction === 'left' ? <LeftArrow /> : <RightArrow />}
+    </Button>
+
+);
+
+
+export default OuterArrow;
--- client/src/components/SimilarHomes.jsx
+++ client/src/components/SimilarHomes.jsx
@@ -2,40 +2,78 @@ import React, {Component} from 'react';
 import styled from 'styled-components';
 import SimilarHome from './SimilarHome.jsx';
 import TextInfo from './TextInfo.jsx'
+import OuterArrow from './OuterArrow.jsx';
 
 const RowDiv = styled.div`
   display: flex;
-  flex-flow: row wrap;
-  justify-content: space-between;
+  flex-direction: row;
+  justify-content: center;
 `;
 
 const ContainerDiv = styled.div`
   display: flex;
   flex-direction: column;
+  margin-right: 25px;
   width: 350px;
   height: 350px;
 `
 
+
 class SimilarHomes extends Component{
   constructor(props){
     super(props);
-    this.state = {}
+    this.state = {
+      currentFirstIndex: 0,
+      currentLastIndex: 2,
+      showLeftButton: false,
+      showRightButton: true
+    }
+    this.previousList = this.previousList.bind(this);
+    this.nextList = this.nextList.bind(this);
+  }
+
+  previousList() {
+    if(this.state.currentFirstIndex > 0) {
+      this.setState(prevState => ({
+        currentFirstIndex: prevState.currentFirstIndex - 1,
+        currentLastIndex: prevState.currentLastIndex - 1
+      }))
+    }
+  }
+
+  nextList() {
+    if(this.state.currentLastIndex < this.props.similarHomes.length - 1){
+      this.setState(prevState => ({
+        currentFirstIndex: prevState.currentFirstIndex + 1,
+        currentLastIndex: prevState.currentLastIndex + 1
+      })
+    )
+  }
   }
 
   render() {
     return(
 
       <RowDiv>
-        {this.props.similarHomes.map((similarHome, i) =>
-          <ContainerDiv key={i}>
-            <SimilarHome images={similarHome.images} />
-            <TextInfo similarHome={similarHome}/>
-          </ContainerDiv>
+      <div>
+      <OuterArrow direction = 'left' handleSubmit = {this.previousList} />
+
+      </div>
+      {this.props.similarHomes.slice(this.state.currentFirstIndex,this.state.currentLastIndex+1).map((similarHome, i) =>
+        <ContainerDiv key={i}>
+        <SimilarHome images={similarHome.images} />
+        <TextInfo similarHome={similarHome}/>
+        </ContainerDiv>
         )}
+        <div>
+        
+        <OuterArrow direction = 'right' handleSubmit = {this.nextList} />
+
+        </div>
       </RowDiv>
 
     )
-    };
+  }
 }
 
 
